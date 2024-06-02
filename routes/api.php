9fa89c2a362f233e657cad0d8945df1cedd4b9ed
njugunamwangi <njugunamwangi.n@gmail.com>
2024-06-02 14:03:01 +0300

    User login login

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Api\AuthController;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,4 +13,4 @@ Route::get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::apiResource('/tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('/tickets', TicketController::class);
